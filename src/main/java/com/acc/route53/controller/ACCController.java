56348071f2b94fc0a59127307cb63ae9ada1308a
happine2s <happine2s@sookmyung.ac.kr>
2025-05-08 04:01:20 +0900

    [fix] controller path

--- src/main/java/com/acc/route53/controller/ACCController.java
+++ src/main/java/com/acc/route53/controller/ACCController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v0")
 public class ACCController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String findUMC() {
         return "hello, world!";
     }
